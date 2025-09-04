068d6385c7e4ad02cefe253f63aa225193046e62
Adriano Andrade <hey.adriano.dev@outlook.com>
2025-09-04 01:33:35 -0300

    Adicionado valor arbitrário no config do tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.tsx',
-  ],
+  content: ["./src/**/*.tsx"],
   theme: {
     extend: {
+      maxWidth: {
+        app: "700px",
+      },
       colors: {
-        diego: '#333'
-      }
+        diego: "#333",
+      },
     },
   },
   plugins: [],
-}
+};
