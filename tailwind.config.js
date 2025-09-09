e08130cabcf5f9ad4624a3b5c5c47005e6614910
Adriano Andrade <hey.adriano.dev@outlook.com>
2025-09-08 23:57:09 -0300

    Cria estrutura de layout com sidebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,13 @@ module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
+      gridTemplateColumns: {
+        app: "minmax(18rem, 20rem) 1fr",
+      },
       maxWidth: {
         app: "700px",
       },
+
       colors: {
         diego: "#333",
       },
