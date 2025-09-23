c6d315ea87884e539acf64dd7e6c1cd54eaa74ca
Adriano Andrade <hey.adriano.dev@outlook.com>
2025-09-22 23:57:39 -0300

    WIP - Listagem de arquivos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -89,6 +89,7 @@ export default function Home() {
             <FileInput.Root className="flex items-start gap-5">
               <FileInput.ImagePreview />
               <FileInput.Trigger />
+              <FileInput.FileList />
               <FileInput.Control />
             </FileInput.Root>
           </div>
